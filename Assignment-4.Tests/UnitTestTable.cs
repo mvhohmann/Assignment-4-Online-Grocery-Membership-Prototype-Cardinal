e56df1ba5008aa7a6a978d0e7ef690398cc85573
Dillon Ewing <djewing1@buffs.wtamu.edu>
2023-03-05 23:00:45 -0600

    TableUnitTest Added

---
+++ Assignment-4.Tests/UnitTestTable.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using Shouldly;
+using Xunit;
+
+namespace UnitTestTable
+{
+    public class Table_Test
+    {
+        [Fact]
+        public async Task Should_Get_Table_of_Orders()
+        {
+            var response = await GetResponseAsStringAsyunc("/DriverHome");
+
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(response);
+
+            var tableElement = htmlDocument.GetElementById("orderTable");
+            tableElement.ShouldNotBeNull();
+
+            var trNodes = tableElement.SelectNodes("//tr");
+            trNodes.Count.ShouldBeGreaterThan(0);
+        }
+    }
+}
